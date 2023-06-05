25bf22ad786f9eb79fe73e43e71b7320a65468a9
TermanEmil <terman.emil@gmail.com>
2023-06-05 14:09:17 +0200

    Change config names because of reserved names on aws

--- handle_bot_message/aws.py
+++ handle_bot_message/aws.py
@@ -7,8 +7,8 @@ import migrations
 dynamodb = boto3.resource(
     'dynamodb',
     region_name='eu-central-1',
-    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'))
+    aws_access_key_id=os.environ.get('CONFIG_AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.environ.get('CONFIG_AWS_SECRET_ACCESS_KEY'))
 
 
 def get_db():
